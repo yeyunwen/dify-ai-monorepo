f3fba9aaa7e99230faa7f74950ee82196bf3be7f
yeyunwen <1571374338@qq.com>
2025-05-21 21:40:35 +0800

    workflow(dify-sdk): add ci test

--- dify-sdk/vitest.config.ts
+++ dify-sdk/vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['__tests__/unit/**/*.test.ts'],
+    setupFiles: ['./__tests__/setupTests.ts'],
     environment: 'node',
-    include: ['**/__tests__/**/*.test.ts'],
     coverage: {
-      enabled: true,
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
